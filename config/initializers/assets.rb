b9e422d9ecfda4c3c5597995e93c9d250409523a
Matthew Vasseur <mattvasseur@gmail.com>
2015-06-16 14:43:19 -0400

    vendor assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,17 +17,35 @@ Rails.application.config.assets.precompile += %w(
 jquery-ui/jquery-ui.min.js
 pace-master/pace.min.js
 jquery-blockui/jquery.blockui.js
-bootstrap/js/bootstrap.min.js
 jquery-slimscroll/jquery.slimscroll.min.js
 switchery/switchery.min.js
 uniform/jquery.uniform.min.js
 classie/classie.js
 waves/waves.min.js
-3d-bold-navigation/js/main.js
 modern.js
 )
 
+# Index Page Assets (broker & policies) (css & js)
 Rails.application.config.assets.precompile += %w(
-pages/table-data.js
+datatables/css/jquery.datatables.css
+datatables/css/jquery.datatables_themeroller.css
+datatables/js/jquery.datatables.min.js
+pages/table-data.js )
+
+# Show Page Assets (policies) (css & js)
+Rails.application.config.assets.precompile += %w(
+x-editable/bootstrap3-editable/css/bootstrap-editable.css
+x-editable/inputs-ext/typeaheadjs/lib/typeahead.js-bootstrap.css
+x-editable/inputs-ext/address/address.css
+select2/css/select2.min.css
+dropzone/dropzone.min.css
+
+moment/moment.js
+x-editable/bootstrap3-editable/js/bootstrap-editable.js
+x-editable/inputs-ext/typeaheadjs/lib/typeahead.js
+x-editable/inputs-ext/typeaheadjs/typeaheadjs.js
+x-editable/inputs-ext/address/address.js
+select2/js/select2.full.min.js
+dropzone/dropzone.min.js
 pages/form-x-editable.js
 )
