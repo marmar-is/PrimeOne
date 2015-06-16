04d2e57d3d49375fffbe2a682203a9ccc863afb8
Matthew Vasseur <mattvasseur@gmail.com>
2015-06-16 18:41:21 -0400

    does it at least show?

--- app/helpers/policies_helper.rb
+++ app/helpers/policies_helper.rb
@@ -8,7 +8,7 @@ module PoliciesHelper
   end
 
   def checkValid(value)
-    if !value.blank? && value.to_i != 0#value != "$-" && value != "-" && value != "0" && value != "$0.00" && !value.blank?
+    if value != nil && !value.blank? && value.to_i != 0#value != "$-" && value != "-" && value != "0" && value != "$0.00" && !value.blank?
       true
     else
       false
