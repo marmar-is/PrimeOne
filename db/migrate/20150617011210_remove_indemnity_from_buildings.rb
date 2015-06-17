982b199e0d45a6fc827ff24d5a503af0f5e2adb3
Matthew Vasseur <mattvasseur@gmail.com>
2015-06-16 21:21:26 -0400

    fix indemnity

--- db/migrate/20150617011210_remove_indemnity_from_buildings.rb
+++
@@ -1,9 +0,0 @@
-class RemoveIndemnityFromBuildings < ActiveRecord::Migration
-  def up
-    remove_column :buildings, :indemnity
-  end
-
-  def down
-    add_column :buildings, :indemnity, :string
-  end
-end
