2143a9112485a45686755e4564bc5663bc63c6db
Matthew Vasseur <mattvasseur@gmail.com>
2015-06-22 21:34:33 -0400

    load forms

--- db/migrate/20150616165709_create_gls.rb
+++ db/migrate/20150616165709_create_gls.rb
@@ -2,13 +2,15 @@ class CreateGls < ActiveRecord::Migration
   def change
     create_table :gls do |t|
       t.decimal :total, default: 0
-      
+
       t.decimal :limit_genagg, default: 0
       t.decimal :limit_products, default: 0
       t.decimal :limit_occurence, default: 0
       t.decimal :limit_injury, default: 0
       t.decimal :limit_fire, default: 0
       t.decimal :limit_medical, default: 0
+
+      t.string :water_gas_tank, default: ""
 
       t.timestamps null: false
     end
