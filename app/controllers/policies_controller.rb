47edb004a2b161648ec05de393752749fa8ff4ae
Matthew Vasseur <mattvasseur@gmail.com>
2015-06-23 20:07:24 -0400

    idk

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -198,9 +198,9 @@ class PoliciesController < ApplicationController
         p.property_forms +=  "CP1030(6/07) "
       end
 
-      if p.locations.first.limit_sign.to_i != 0 && !p.property_forms.include?("CP1440(6/07) ")
-        p.property_forms +=  "CP1440(6/07) "
-      end
+      #if p.locations.first.limit_sign.to_i != 0 && !p.property_forms.include?("CP1440(6/07) ")
+      #  p.property_forms +=  "CP1440(6/07) "
+      #end
 
       if p.locations.first.enhancement.to_i != 0 && !p.property_forms.include?("PO-PRP-3(12/13) ")
         p.property_forms +=  "PO-PRP-3(12/13) "
